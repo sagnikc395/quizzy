083488a64543761c93649a710c4fb136a8356345
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-28 07:43:38 +0530

    cleanup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
-import http from "http";
 import { IOManager } from "./managers/IOManager";
-
-const server = http.createServer();
 
 const io = IOManager.getIo();
 
 io.on("connection", (client) => {
   client.on("event", (data) => {
     const type = data.type;
+    //3 admin events 
+    //2 client events
+    //QuizManager => Quiz => broadcast
+
   });
 
   client.on("disconnect", () => {});
